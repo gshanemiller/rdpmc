0b3b10fc343d311b566c11512177f9a16aa4d3ba
Your Name <you@example.com>
2022-05-05 04:19:19 +0000

    remove old code

--- src/rdpmc.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <sys/types.h>                                                                                                  
-                                                                                                                        
-int rdpmc_initialize(int cpu);
-u_int64_t rdpmc_readCounter(int counter);
